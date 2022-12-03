8ec3ad62b1388b575321eb2596c831de6454a746
mystery000 <lkostyrka17@gmail.com>
2022-12-03 19:37:44 +0200

    feat: Make the loop for all directories

--- P.py
+++ P.py
@@ -4,6 +4,7 @@ import sys
 import statistics
 import numpy as np
 from scipy import stats
+from pathlib import Path
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
@@ -45,7 +46,14 @@ select_col = [
   ,"darkslategray"
   ,"chartreuse"
   ,"darkred"
-  ,"orangered"       
+  ,"orangered"      
+  ,"springgreen"
+  ,"orchid"
+  ,"blue"
+  ,"darkslategray"
+  ,"chartreuse"
+  ,"darkred"
+  ,"orangered" 
 ]
 window = 51
 step = 10
@@ -53,62 +61,6 @@ step = 10
 os.chdir(f"{os.getcwd()}\Eau_Physio_248mV")
 savedir = f"{os.getcwd()}"
 
-angle = [
-  "0Deg"
-  ,"9Deg"
-  ,"18Deg"
-  ,"27Deg"
-  ,"36Deg"
-  ,"45Deg"
-  ,"54Deg"
-  ,"72Deg"
-  ,"81Deg"
-  ,"90Deg"
-  ,"99Deg"
-  ,"108Deg"
-  ,"117Deg"
-  ,"126Deg"
-  ,"135Deg"
-  ,"144Deg"
-  ,"153Deg"
-  ,"162Deg"
-  ,"171Deg"
-  ,"180Deg"
-]
-tension =[
-   "2400"
-  ,"2410"
-  ,"2420"
-  ,"2430"
-  ,"2440"
-  ,"2450"
-  ,"2460"
-  ,"2470"
-  ,"2480"        
-]
-manip = [
-          "Eau_Physio_248mV"
-          ,"AIR A 2490mV"
-          ,"1Abs_268mV_0mA_Ispec850"
-          ,"2Abs_288mV_0mA_Ispec850"
-          ,"4Abs_3260mV_9mA_Ispec850"
-          ,"12Abs_3600mV_23mA_Ispec380"
-          ,"24Abs_3600mV_23mA_Ispec200"
-          ,"36Abs_3600mV_23mA_Ispec"
-          ,"40Abs_3600mV_23mA_Ispec"        
-]
-prefix = [
-          "/Eau_Physio_248mV_"
-          ,"/AIR_"
-          ,"/1Abs_268mV_0mA_Ispec850_"
-          ,"/2Abs_288mV_0mA_Ispec850_"
-          ,"/4Abs_3260mV_9mA_Ispec850_"
-          ,"/12Abs_3600mV_23mA_Ispec380_"
-          ,"/24Abs_3600mV_23mA_Ispec200_"
-          ,"/36Abs_3600mV_23mA_Ispec_"
-          ,"/40Abs_3600mV_23mA_Ispec_"
-]
-postfix = "_IntegT900E-3.LVM"
 
 def convert(e):
     x = e[0]
@@ -128,8 +80,8 @@ def extractV1(e):
 def extractV3(e):
     return e[2]
 
-def input(i_angle, i_manip):
-    file = "".join([savedir,"/",manip[i_manip],prefix[i_manip],angle[i_angle],postfix])
+def input(d, f):
+    file = "".join([savedir,"/",d, "/", f])
     print(file)
     with open(file, newline='') as f:
         reader = csv.reader(f)
@@ -145,21 +97,29 @@ def input(i_angle, i_manip):
 
 pdf = PdfPages("".join([savedir,"/plots.pdf"]))
 
-for i_manip in range(0, 9):   
-    fig = plt.figure(figsize = (15,15))
-    for i_angle in range(0, 20):
-        data = input(i_angle, i_manip)
+dirs = []
+for dir in os.listdir():
+    if Path(f"{os.getcwd()}/{dir}").is_dir():
+        dirs.append(dir)
+
+for dir in dirs:   
+    fig = plt.figure(figsize = (20,20))
+    files = []
+    for (root,folder,file) in os.walk(dir):
+        files = file
+        break
+    for i, file in enumerate(files):
+        data = input(dir, file)
         n = data[0]
         w1 = data[1]
         spectre = data[2]
-        plt.subplot(5,4,i_angle+1).set_ylim(0,1)
-        plt.tight_layout()
-        plt.plot(w1, spectre,'ro', color=select_col[i_angle], markersize=1)
+        plt.subplot(5,5,i+1).set_ylim(0,1)
+        plt.tight_layout(pad=0.5, w_pad=0.5, h_pad=1.0)
+        plt.plot(w1, spectre,'ro', color=select_col[i], markersize=1)
         plt.grid()
-        plt.title(angle[i_angle])        
-    plt.suptitle(manip[i_manip], color="Black", y=1.05, fontsize="xx-large")
+        plt.title(file[:20]+"...")        
+    plt.suptitle(dir, color="Black", y=1.05, fontsize="xx-large")
     fig.savefig(pdf, format="pdf", bbox_inches="tight")
     sys.stdout.flush() 
     
 pdf.close()
-
